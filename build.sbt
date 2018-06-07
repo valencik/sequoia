015439d9495e63c4fc61c822671ff8b1d8bef676
Andrew Valencik <andrew.valencik@gmail.com>
2018-06-07 16:40:35 -0400

    Make equivalent Presto SQL app

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 lazy val ScalaTestVersion  = "3.0.5"
-lazy val sparkVersion = "2.3.0"
+lazy val prestoVersion = "0.203"
 
 lazy val root = (project in file(".")).
   settings(
@@ -14,31 +14,7 @@ lazy val root = (project in file(".")).
     name := "Big SQL Parse",
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % ScalaTestVersion % Test,
-      "org.apache.spark" %% "spark-core"     % sparkVersion,
-      "org.apache.spark" %% "spark-sql"      % sparkVersion
+      "com.facebook.presto" % "presto-parser" % prestoVersion,
+      "org.antlr" % "antlr4-runtime" % "4.6"
     )
-  ).settings(repl: _*)
-
-lazy val repl = Seq(
-  initialize ~= { _ => // Color REPL
-    val ansi = System.getProperty("sbt.log.noformat", "false") != "true"
-    if (ansi) System.setProperty("scala.color", "true")
-  },
-  initialCommands in console :=
-    """
-      |import org.apache.spark.{SparkConf, SparkContext}
-      |import org.apache.spark.sql.SparkSession
-      |
-      |val conf = new SparkConf().setMaster("local[*]").setAppName("repl").set("spark.ui.enabled", "false")
-      |implicit val spark = SparkSession.builder().config(conf).appName("REPL").getOrCreate()
-      |
-      |import spark.implicits._
-      |
-      |spark.sparkContext.setLogLevel("WARN")
-      |
-    """.stripMargin,
-  cleanupCommands in console :=
-    """
-      |spark.stop()
-    """.stripMargin
-)
+  )
--- src/main/scala/ca/valencik/bigsqlparse/ParseBuddy.scala
+++ src/main/scala/ca/valencik/bigsqlparse/ParseBuddy.scala
@@ -1,24 +1,29 @@
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import com.facebook.presto.sql.parser.SqlParser
+import com.facebook.presto.sql.parser.ParsingOptions
+import com.facebook.presto.sql.parser.ParsingOptions.DecimalLiteralTreatment.AS_DOUBLE
+import com.facebook.presto.sql.tree.Statement
 
-object ParseBuddyApp extends App {
+object PrestoBuddyApp extends App {
 
-  val conf = new SparkConf().setMaster("local[*]").setAppName("repl").set("spark.ui.enabled", "false")
-  implicit val spark = SparkSession.builder().config(conf).appName("REPL").getOrCreate()
-  spark.sparkContext.setLogLevel("WARN")
+  private val exitCommands = Seq("exit", ":q", "q")
+  def exitCommand(command: String): Boolean = exitCommands.contains(command.toLowerCase)
 
-  import spark.implicits._
+  private val prestSqlParser = new SqlParser()
 
-  def parse(query: String): LogicalPlan = {
-    spark.sessionState.sqlParser.parsePlan(query)
+  def parsePrestoSql(query: String): Statement = {
+    val parsingOptions = new ParsingOptions(AS_DOUBLE /* anything */)
+    val statement = prestSqlParser.createStatement(query, parsingOptions)
+    statement
   }
 
-  val inputQuery = scala.io.StdIn.readLine("ParseBuddy> ")
-  val lp = parse(inputQuery)
-  println(lp)
-  println(lp.prettyJson)
+  def prestoLoop(): Unit = {
+    val inputQuery = scala.io.StdIn.readLine("ParseBuddy> ")
+    if (!exitCommand(inputQuery)) {
+      val lp = parsePrestoSql(inputQuery)
+      println(lp)
+      prestoLoop()
+    }
+  }
 
-
-  spark.stop()
+  prestoLoop()
 }
