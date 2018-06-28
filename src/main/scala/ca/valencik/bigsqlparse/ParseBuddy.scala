3e0bf47802a3c0196c4af25b4f10f8ac6a12f264
Andrew Valencik <andrew.valencik@gmail.com>
2018-06-27 23:14:56 -0400

    Add support for comparison expressions in joins!

--- src/main/scala/ca/valencik/bigsqlparse/ParseBuddy.scala
+++ src/main/scala/ca/valencik/bigsqlparse/ParseBuddy.scala
@@ -15,11 +15,25 @@ case class Name(text: String, location: NodeLocation) extends Node with Relation
 case class SelectItem(location: NodeLocation) extends Node
 case class Select(selectItems: List[String], location: NodeLocation) extends Node
 
-sealed trait Relation extends Node
+sealed trait Relation
 case class From(relations: List[Relation]) extends Node
 
 sealed trait Expression
 case class Identifier(name: String, location: NodeLocation) extends Node with Expression
+case class BooleanExpression(left: Expression, op: Operator, right: Expression) extends Node with Expression
+case class ComparisonExpression(left: Expression, op: Comparison, right: Expression) extends Node with Expression
+
+sealed trait Operator
+case object AND extends Operator
+case object OR extends Operator
+
+sealed trait Comparison
+case object EQ extends Comparison
+case object NEQ extends Comparison
+case object LT extends Comparison
+case object LTE extends Comparison
+case object GT extends Comparison
+case object GTE extends Comparison
 
 case class Where(expression: Option[Expression]) extends Node
 case class GroupingElement(groupingSet: List[Expression]) extends Node
@@ -119,7 +133,7 @@ class PrestoSqlVisitorApp extends SqlBaseBaseVisitor[Node] {
     else {
       val jc = ctx.joinCriteria
       if (jc.ON != null)
-        Some(JoinOn(visit(ctx.joinCriteria.booleanExpression).asInstanceOf[Identifier]))
+        Some(JoinOn(visit(ctx.joinCriteria.booleanExpression).asInstanceOf[Expression]))
       else if (jc.USING != null)
         Some(JoinUsing(ctx.joinCriteria().identifier.map{
           case ic => Identifier(ic.getText, NodeLocation(ic.getStart))
@@ -130,8 +144,30 @@ class PrestoSqlVisitorApp extends SqlBaseBaseVisitor[Node] {
   }
 
   def getRight(ctx: SqlBaseParser.JoinRelationContext): Relation = {
-    // TODO FIX HACK
-    SampledRelation("RIGHT" + ctx.getText, NodeLocation(ctx.getStart))
+    val rel = if (ctx.CROSS != null)
+      visit(ctx.right)
+    else if (ctx.NATURAL != null)
+      visit(ctx.right)
+    else
+      visit(ctx.rightRelation)
+    rel.asInstanceOf[Relation]
+  }
+
+  def getComparisonOperator(ctx: SqlBaseParser.ComparisonOperatorContext): Comparison = {
+    if (ctx.EQ != null)
+      EQ
+    else if (ctx.NEQ != null)
+      NEQ
+    else if (ctx.GT != null)
+      GT
+    else if (ctx.LT != null)
+      LT
+    else if (ctx.GTE != null)
+      GTE
+    else if (ctx.LTE != null)
+      LTE
+    else
+      ???
   }
 
   override def visitQueryNoWith(ctx: SqlBaseParser.QueryNoWithContext) = {
@@ -156,6 +192,23 @@ class PrestoSqlVisitorApp extends SqlBaseBaseVisitor[Node] {
     Join(joinType, left, right, NodeLocation(ctx.getStart), joinCriteria)
   }
 
+  override def visitPredicated(ctx: SqlBaseParser.PredicatedContext) = {
+    if (ctx.predicate != null)
+      visit(ctx.predicate)
+    else
+      visit(ctx.valueExpression)
+  }
+  override def visitComparison(ctx: SqlBaseParser.ComparisonContext) = {
+    val op = getComparisonOperator(ctx.comparisonOperator)
+    ComparisonExpression(visit(ctx.value).asInstanceOf[Expression], op, visit(ctx.right).asInstanceOf[Expression])
+  }
+  override def visitLogicalBinary(ctx: SqlBaseParser.LogicalBinaryContext) = {
+    val op = if (ctx.AND != null) AND else OR
+    BooleanExpression(visit(ctx.left).asInstanceOf[BooleanExpression], op, visit(ctx.right).asInstanceOf[BooleanExpression])
+  }
+  override def visitValueExpressionDefault(ctx: SqlBaseParser.ValueExpressionDefaultContext) = {
+    Identifier(ctx.getText, NodeLocation(ctx.getStart))
+  }
   override def visitQualifiedName(ctx: SqlBaseParser.QualifiedNameContext) = {
     Name(ctx.getText, NodeLocation(ctx.getStart))
   }
