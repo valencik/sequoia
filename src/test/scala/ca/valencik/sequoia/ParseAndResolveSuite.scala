fcf21ef54ac12088c238ffbb2a89cdf362b4afa5
Andrew Valencik <andrew.valencik@gmail.com>
2019-12-01 23:46:34 -0500

    Add basic test for parsing and resolving

---
+++ src/test/scala/ca/valencik/sequoia/ParseAndResolveSuite.scala
@@ -0,0 +1,34 @@
+package ca.valencik.sequoia
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ParseAndResolveSuite extends AnyFlatSpec with Matchers {
+
+  import ca.valencik.sequoia.ParseBuddy
+  import ca.valencik.sequoia.MonadSqlState._
+  import ca.valencik.sequoia._
+
+  val catalog    = Catalog(Map("db" -> List("a")))
+  val emptyState = Resolver()
+
+  "ParseAndResolver" should "resolve simple queries from catalog" in {
+    val parsedQuery = ParseBuddy.parse("select a from db")
+    val (log, finalState, rq) =
+      resolveQuery(parsedQuery.right.get).value.run(catalog, emptyState).value
+
+    log.isEmpty shouldBe false
+    finalState shouldBe emptyState.addRelationToScope("db", List("a")).addColumnToProjection("a")
+    rq.isRight shouldBe true
+  }
+
+  it should "not resolve simple queries not from catalog" in {
+    val parsedQuery = ParseBuddy.parse("select a from foo")
+    val (log, finalState, rq) =
+      resolveQuery(parsedQuery.right.get).value.run(catalog, emptyState).value
+
+    log.isEmpty shouldBe false
+    finalState shouldBe emptyState
+    rq.isRight shouldBe false
+  }
+}
