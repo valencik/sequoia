1ea0a39fe274672bb807e8572e193e904321f083
Andrew Valencik <andrew.valencik@gmail.com>
2020-05-14 21:13:57 -0400

    Add Optional and Traversal optics

--- modules/rewrite/src/main/scala/ca/valencik/sequoia/Lenses.scala
+++ modules/rewrite/src/main/scala/ca/valencik/sequoia/Lenses.scala
@@ -1,47 +1,84 @@
 package ca.valencik.sequoia
 
-import monocle.Prism
+import monocle.Lens
+import monocle.Optional
+import monocle.Traversal
 
 object Lenses {
 
-  private def anonTableName[R](rawName: R): TableName[Unit, R] =
-    TableName((), TableRef((), rawName))
+  def tnTable[I, R]: Lens[TableName[I, R], R] =
+    Lens[TableName[I, R], R](_.ref.value)(r => tn => tn.copy(ref = tn.ref.copy(value = r)))
 
-  private def anonRelation[R](rawName: R): Relation[Unit, R] = {
-    val tn = anonTableName(rawName)
-    val ar = AliasedRelation((), tn, tableAlias = None, columnAliases = None)
-    SampledRelation((), aliasedRelation = ar, tableSample = None)
+  def rpSampledR[I, R]: Lens[SampledRelation[I, R], RelationPrimary[I, R]] =
+    Lens[SampledRelation[I, R], RelationPrimary[I, R]](_.aliasedRelation.relationPrimary) {
+      rp => sr => sr.copy(aliasedRelation = sr.aliasedRelation.copy(relationPrimary = rp))
+    }
+
+  def relJoinR[I, R]: Traversal[JoinRelation[I, R], Relation[I, R]] = {
+    def getLeft(jr: JoinRelation[I, R]): Relation[I, R]  = jr.left
+    def getRight(jr: JoinRelation[I, R]): Relation[I, R] = jr.right
+    Traversal.apply2(getLeft, getRight) {
+      case (left, right, jr) => jr.copy(left = left, right = right)
+    }
   }
 
-  def tnRelation[R]: Prism[Relation[_, R], R] =
-    Prism[Relation[_, R], R] {
-      case jr: JoinRelation[_, R] => tnRelation.getOption(jr.left)
-      case sr: SampledRelation[_, R] =>
+  def tnRelation[I, R]: Optional[Relation[I, R], R] =
+    Optional[Relation[I, R], R] {
+      case jr: JoinRelation[I, R] => tnRelation.getOption(jr.left)
+      case sr: SampledRelation[I, R] =>
         tnRelationPrimary.getOption(sr.aliasedRelation.relationPrimary)
-    }(anonRelation)
+    } { rawName => relation =>
+      relation match {
+        case sr: SampledRelation[I, R] => {
+          val rp: RelationPrimary[I, R] =
+            tnRelationPrimary.set(rawName)(sr.aliasedRelation.relationPrimary)
+          val newAR: AliasedRelation[I, R] = sr.aliasedRelation.copy(relationPrimary = rp)
+          sr.copy(aliasedRelation = newAR)
+        }
+        case _ => relation
+      }
+    }
 
-  def tnRelationPrimary[R] =
-    Prism[RelationPrimary[_, R], R] {
-      case tn: TableName[_, R]             => Some(tn.ref.value)
-      case _: SubQueryRelation[_, R]       => None
-      case _: Unnest[_, R]                 => None
-      case _: LateralRelation[_, R]        => None
-      case pr: ParenthesizedRelation[_, R] => tnRelation.getOption(pr.relation)
-    }(anonTableName)
+  def tnRelationPrimary[I, R]: Optional[RelationPrimary[I, R], R] =
+    Optional[RelationPrimary[I, R], R] {
+      case tn: TableName[I, R]             => Some(tn.ref.value)
+      case _: SubQueryRelation[I, R]       => None
+      case _: Unnest[I, R]                 => None
+      case _: LateralRelation[I, R]        => None
+      case pr: ParenthesizedRelation[I, R] => tnRelation.getOption(pr.relation)
+    } { rawName => relation =>
+      relation match {
+        case tn: TableName[I, R]            => tnTable.set(rawName)(tn)
+        case _: ParenthesizedRelation[I, R] => relation
+        case _                              => relation
+      }
+    }
+
 }
 
 object LensApp {
 
   import Lenses._
+  def simpleRelation(name: String) =
+    SampledRelation(1, AliasedRelation(2, TableName(3, TableRef(4, name)), None, None), None)
+
+  def joinRelation(left: String, right: String) =
+    JoinRelation(1, InnerJoin, simpleRelation(left), simpleRelation(right), None)
 
   def main(args: Array[String]): Unit = {
-    val relation =
-      SampledRelation(1, AliasedRelation(2, TableName(3, TableRef(4, "foo")), None, None), None)
-    val x = tnRelation.getOption(relation)
-    println(x)
 
-    // TODO: We are losing all our Info params here
-    val y = tnRelation[String].modify(_.toUpperCase)(relation)
+    val foo    = simpleRelation("foo")
+    val foobar = joinRelation("foo", "bar")
+
+    val maybeFoo  = tnRelation.getOption(foo)
+    val maybeFoo2 = tnRelation.getOption(foobar)
+    println(maybeFoo)
+    println(maybeFoo2)
+
+    val upperFoo = tnRelation[Int, String].modify(_.toUpperCase)(foo)
+    println(upperFoo)
+
+    val y = relJoinR[Int, String].getAll(foobar).map(tnRelation.getOption)
     println(y)
   }
 }
